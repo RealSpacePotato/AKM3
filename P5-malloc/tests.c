7660338314a269e24b8bb9031a5ca6e84ddbbd83
RealSpacePotato <30611977+RealSpacePotato@users.noreply.github.com>
2017-11-24 20:38:35 -0500

    last part 1 udpaet

--- P5-malloc/tests.c
+++ P5-malloc/tests.c
@@ -19,10 +19,10 @@ const char* test_descriptions[] = {
     /* 4  */ "resize with null block pointer should act as alloc",
     /* 5  */ "release with null block pointer should act as NOP",
     /* 6  */ "alloc return address 8-byte aligned",
-    /* 7  */ "resize change location of block when not possible to increase the size of the current block but there is room elsewhere",
+    /* 7  */ "alloc, release, alloc should not return null pointer",
     /* 8  */ "When resizing to smaller size, contents inside are the same",
     /* 9  */ "When resize changes location, contents should be preserved",
-    /* 10 */ "your description here",
+    /* 10 */ "resize return address 8-byte aligned",
     /* 11 */ "your description here",
     /* 12 */ "your description here",
     /* 13 */ "your description here",
@@ -30,13 +30,13 @@ const char* test_descriptions[] = {
     /* 15 */ "your description here",
     /* STRESS tests */
     /* 16 */ "alloc & free, stay within heap limits",
-    /* 17 */ "your description here",
-    /* 18 */ "your description here",
-    /* 19 */ "your description here",
-    /* 20 */ "your description here",
+    /* 17 */ "Structural Integrity - alloc and resize",
+    /* 18 */ "Structural Integrity - alloc and release",
+    /* 19 */ "Data Integrity - data preservation - alloc and release",
+    /* 20 */ "Data Integrity - data preservation - alloc and resize",
     /* 21 */ "your description here",
-    /* 22 */ "checks to see if an alloc will return a block of memory that runs past end of heap",
-    /* 23 */ "alloc following release should not reuse released memory unless big enough to fit requested bytes",
+    /* 22 */ "your description here",
+    /* 23 */ "your description here",
 };
 
 /* -------------------- PRINT DEBUG FNS ----------------- */
@@ -54,7 +54,6 @@ const char* test_descriptions[] = {
 	#define DEBUG_PRINT_2(fmt, v1, v2)
 	#define DEBUG_PRINT_3(fmt, v1, v2, v3)
 #endif
-
 
 /* ------------------ COMPLETED SPEC TESTS ------------------------- */
 
@@ -178,8 +177,9 @@ int test03() {
         else
             break;
     }
-
-    DEBUG_PRINT_2("%d blocks (n), then %d blocks (2n) allocated\n", num_blocks, num_blocks2);
+#ifdef PRINT_DEBUG
+    printf("%d blocks (n), then %d blocks (2n) allocated\n", num_blocks, num_blocks2);
+#endif
 
     // hoping to return SUCCESS (==1)
     return (num_blocks2 > num_blocks);
@@ -192,11 +192,11 @@ int test03() {
  *
  * FUNCTIONS BEING TESTED: resize
  * SPECIFICATION BEING TESTED: When a null pointer is passed into the block pointer,
- * resize should work as hl_alloc
+ * resize should work as hl_alloc.
  *
  * MANIFESTATION OF ERROR:
  * See if hl_resize acts as hl_alloc when block pointer is NULL. If hl_resize returns a NULL pointer,
- * it should fail. 
+ * it should fail.
  */
 int test04() {
     
@@ -210,11 +210,12 @@ int test04() {
  * for that thing!
  *
  * FUNCTIONS BEING TESTED: release
- * SPECIFICATION BEING TESTED: 
+ * SPECIFICATION BEING TESTED:
  * When release is called with a null block-pointer, it should act as a NOP
  *
  * MANIFESTATION OF ERROR:
- * 
+ * This test is designed to see with release with a null pointer acts as a NOP. If hl_release releases
+ * some block in memory, the library is flawed. 
  */
 int test05() {
     char heap[HEAP_SIZE];
@@ -228,12 +229,12 @@ int test05() {
 /* Find something that you think heaplame does wrong. Make a test
  * for that thing!
  *
- * FUNCTIONS BEING TESTED:
+ * FUNCTIONS BEING TESTED: alloc
  * SPECIFICATION BEING TESTED:
- *
+ * The return address of hl_alloc is 8-byte aligned.
  *
  * MANIFESTATION OF ERROR:
- *
+ * If the return address of hl_alloc is not 8-byte aligned, the library is flawed.
  */
 int test06() {
     char heap[HEAP_SIZE];
@@ -247,50 +248,37 @@ int test06() {
 /* Find something that you think heaplame does wrong. Make a test
  * for that thing!
  *
- * FUNCTIONS BEING TESTED: hl_resize()
- * SPECIFICATION BEING TESTED: hl_resize() should change the location of a
- * block if it is not possible to resize at current location, but there is
- * room elsewhere.
- *
+ * FUNCTIONS BEING TESTED: alloc, release
+ * SPECIFICATION BEING TESTED:
+ * hl_alloc after a release should not return a null pointer.
  *
  * MANIFESTATION OF ERROR:
- * 1. address of new block has not changed
- *
+ * If hl_alloc returns a null pointer after a release, the library is flawed.
  */
 int test07() {
-	
+
     char heap[HEAP_SIZE];
     hl_init(heap, HEAP_SIZE);
 
-    char *array = hl_alloc(heap, HEAP_SIZE/4);
-	array[0] = '1';
-	
-    char *newarray = hl_alloc(heap, HEAP_SIZE/4);
-	newarray[0] = '2';
-    
-	DEBUG_PRINT_1("array: %p\n", array);
-	DEBUG_PRINT_1("newarray: %p\n", newarray);
+    char *array = hl_alloc(heap, (3*HEAP_SIZE)/4);
+    hl_release(heap, array);
 
-	// this resize should force the block to a new location past newarray
-    char *resizearray = hl_resize(heap, array, HEAP_SIZE/3);
-    
-	DEBUG_PRINT_1("resizearray: %p\n", resizearray);
-    
-    return (array != resizearray);
+    return (hl_alloc(heap, (3*HEAP_SIZE)/4) != NULL);
+
 }
 
 /* Find something that you think heaplame does wrong. Make a test
  * for that thing!
  *
- * FUNCTIONS BEING TESTED:
+ * FUNCTIONS BEING TESTED: alloc, resize
  * SPECIFICATION BEING TESTED:
- *
+ * hl_resize should not change the contents of the array returned by hl_alloc
  *
  * MANIFESTATION OF ERROR:
- *
+ * If the contents differ for arrays returned by hl_resize and hl_alloc, the library is flawed.
  */
 int test08() {
-     
+
     char heap[HEAP_SIZE];
     hl_init(heap, HEAP_SIZE);
 
@@ -301,13 +289,13 @@ int test08() {
     }
 
     char *newarray = (char *) hl_resize(heap, array, HEAP_SIZE/8);
-    
+
     bool testfail = SUCCESS;
 
     for (int j=0; j< HEAP_SIZE/8; j++) {
-		if (array[j] != newarray[j]) {
-			testfail = NULL;
-		}
+	if (array[j] != newarray[j]) {
+	    testfail = NULL;
+	}
     }
 
     return testfail;
@@ -316,12 +304,12 @@ int test08() {
 /* Find something that you think heaplame does wrong. Make a test
  * for that thing!
  *
- * FUNCTIONS BEING TESTED:
+ * FUNCTIONS BEING TESTED: alloc, resize
  * SPECIFICATION BEING TESTED:
- *
+ * When resizes changes location of pointer in heap, the contents should be preserved.
  *
  * MANIFESTATION OF ERROR:
- *
+ * If the contents differ after resize is called, the library is flawed. 
  */
 int test09() {
 
@@ -332,72 +320,47 @@ int test09() {
 
     for(int i=0; i< (HEAP_SIZE/8)/sizeof(char) ; i++) {
 	arrayA[i]= 'A';
-		if (i == 0) {
-			DEBUG_PRINT_1("address of arrayA[0] = %p\n", &arrayA[0]);
-			DEBUG_PRINT_1("value of arrayA[0] = %c\n ", arrayA[0]);
-        }
-	DEBUG_PRINT_2("%d : %c \n", i, arrayA[i]);
     }
 
     char *arrayB = (char*) hl_alloc(heap, HEAP_SIZE/8);
 
     for (int j=0; j< (HEAP_SIZE/8)/sizeof(char); j++) {
-		arrayB[j] = 'B';
-		DEBUG_PRINT_2("%d : %c \n", j, arrayA[j]);
+	arrayB[j] = 'B';
     }
 
-    //for (int t=0; t < HEAP_SIZE; t++) {
-	//DEBUG_PRINT_2("%d: %c \n", t, heap[t]);
-    //}
-
-    DEBUG_PRINT_1("pointer for arrayA before resize: %p \n", arrayA);
-    DEBUG_PRINT_1("before resize: arrayA[0] = %c \n", arrayA[0]);
-    DEBUG_PRINT_1("before resize arrayA[0] address = %p \n", &arrayA[0]);
-    DEBUG_PRINT_1("pointer for arrayB before resize: %p \n", arrayB);
-
     char *resizeArray = (char *) hl_resize(heap, arrayA, HEAP_SIZE/4);
-    
-    DEBUG_PRINT_1("pointer for arrayA: %p \n", arrayA);
-    DEBUG_PRINT_1("arrayA[0] = %c \n", arrayA[0]);
-    DEBUG_PRINT_1("pointer for arrayB: %p \n", arrayB);
-    DEBUG_PRINT_1("pointer for resizeArray:  %p \n", resizeArray);
-    DEBUG_PRINT_1("resizeArray[0] = %c \n", resizeArray[0]);
-    
+
     bool testfail = SUCCESS;
 
-    //for (int u =0; u < HEAP_SIZE; u++) {
-	//	DEBUG_PRINT_2("%d: %c \n", u, heap[u]);
-    //}
-    
-    //resizeArray[0] = arrayA[0];
-    
     for (int k=0; k < (HEAP_SIZE/8)/sizeof(char); k++) {
-		DEBUG_PRINT_3("address %p: %d : %c \n", &resizeArray[k], k, resizeArray[k]);
-		if (resizeArray[k] != 'A'){
-			testfail = NULL;
-			DEBUG_PRINT_1("testfail changed to %d \n", testfail);
-		}
+	if (resizeArray[k] != 'A'){
+	  testfail = NULL;
+	}
      }
-    
-    //for (int u=0; u<HEAP_SIZE; u++) {
-	//DEBUG_PRINT_2("%d: %c \n", u, heap[u]);
-    //}
 
-    DEBUG_PRINT_1("testfail =  %d \n", testfail);
-    
     return testfail;
 }
 
 /* Find something that you think heaplame does wrong. Make a test
  * for that thing!
  *
- * FUNCTIONS BEING TESTED: 
- * SPECIFICATION BEING TESTED: 
+ * FUNCTIONS BEING TESTED: resize
+ * SPECIFICATION BEING TESTED:
+ * The return address of resize should be 8-byte aligned.
  *
- * MANIFESTATION OF ERROR: 
+ * MANIFESTATION OF ERROR:
+ * If the return address of resize is not 8-byte aligned, the library is flawed.
  */
 int test10() {
-    return FAILURE;
+
+    char heap[HEAP_SIZE];
+    hl_init(heap, HEAP_SIZE);
+
+    int *array = hl_alloc(heap,HEAP_SIZE/2);
+    
+    int *newarray = hl_resize(heap, array, (3*HEAP_SIZE)/4);
+
+    return (((uintptr_t) newarray) % 8 == 0);
 }
 
 /* Find something that you think heaplame does wrong. Make a test
@@ -412,7 +375,7 @@ int test10() {
  */
 int test11() {
 
-    return FAILURE;
+     return FAILURE;
 }
 
 /* Find something that you think heaplame does wrong. Make a test
@@ -492,7 +455,7 @@ int test15() {
  *
  */
 int test16() {
-    int n_tries    = 10000;
+int n_tries    = 10000;
     int block_size = 220;
 
     // 1024 bytes of padding--fill it with Z's, check at end for change
@@ -504,7 +467,7 @@ int test16() {
 
     char *heap_start = &memarea[HEAP_SIZE]; // heap will start 1024 bytes in
     char *pointers[NPOINTERS];
-	
+
 	// fill padding areas above and below heap with values we can 
 	// check at the end to see if any were overwritten
 	memset(memarea, 'A', HEAP_SIZE);
@@ -529,21 +492,21 @@ int test16() {
             pointers[index] = 0;
         }
     }
-	
+
 	// OK after all that allocating, releasing and writing, check
 	// to see if we ever wrote beyond the top or bottom of the memory
 	// area allocated for the heap.
 	bool overwrite_below = 0;
 	bool overwrite_above = 0;
-	
+
 	// check area below the heap
 	for (int i = 0; i < HEAP_SIZE && overwrite_below == 0; i++) {
 		overwrite_below = (memarea[i] != 'A');
 		if (overwrite_below) {
 			DEBUG_PRINT_3("Memory below heap overwritten. HeapStart: %p, Written Addr: %p, Written Val: %c\n", heap_start, &memarea[i], memarea[i]);
-		}	
+		}
 	}
-	
+
 	// check area above the heap
 	for (int i = HEAP_SIZE*2; i < HEAP_SIZE*3 && overwrite_above == 0; i++) {
 		overwrite_above = (memarea[i] != 'Z');
@@ -551,61 +514,199 @@ int test16() {
 			DEBUG_PRINT_3("Memory above heap overwritten. HeapStart: %p, Written Addr: %p, Written Val: %c\n", heap_start, &memarea[i], memarea[i]);
 		}
 	}
-	
+
     return (!overwrite_above && !overwrite_below);
 }
 
 /* Stress the heap library and see if you can break it!
  *
- * FUNCTIONS BEING TESTED: 
- * INTEGRITY OR DATA CORRUPTION?
- *
+ * FUNCTIONS BEING TESTED: alloc and resize
+ * INTEGRITY OR DATA CORRUPTION? STRUCTURAL INTEGRITY
+ * 
  * MANIFESTATION OF ERROR:
- *
+ * At some point a field in the heap thought to contain a pointer will no longer contain a valid address.
  */
 int test17() {
 
-    return FAILURE;
+    int n_tries = 10000;
+
+    char heap[HEAP_SIZE*3];
+
+    char *pointers[NPOINTERS];
+
+    //zero out the pointer array
+    memset(pointers, 0, NPOINTERS*sizeof(char *));
+
+    hl_init(heap, HEAP_SIZE*3);
+
+    srandom(0);
+    for (int i =0; i < n_tries; i++) {
+	int index = random() % NPOINTERS;
+	if (pointers[index] == 0) {
+	     //NULL pointer, set pointer to heap
+	     pointers[index] = hl_alloc(heap, random() % (HEAP_SIZE*2));
+	} else {
+	    //resize the size of heap
+	    pointers[index] = hl_resize(heap, pointers[index],random() % (HEAP_SIZE*2));
+	}
+    }
+
+    return SUCCESS;
 }
 
 /* Stress the heap library and see if you can break it!
  *
- * FUNCTIONS BEING TESTED:
- * INTEGRITY OR DATA CORRUPTION?
+ * FUNCTIONS BEING TESTED: alloc, release
+ * INTEGRITY OR DATA CORRUPTION? STRUCTURAL INTEGRITY
  *
  * MANIFESTATION OF ERROR:
- *
+ * At some point a field in the heap thought to contain a pointer will no longer contain a valid address.
  */
 int test18() {
 
-    return FAILURE;
+    int n_tries = 10000;
+
+    char heap[HEAP_SIZE*3];
+    char *pointers[NPOINTERS];
+
+    memset(pointers, 0, NPOINTERS*sizeof(char *));
+
+    hl_init(heap, HEAP_SIZE*3);
+
+    srandom(0);
+    for (int i =0; i < n_tries; i++) {
+	int index = random() % NPOINTERS;
+	if (pointers[index] == 0) {
+	    pointers[index] = hl_alloc(heap, random() % (HEAP_SIZE*2));
+	} else {
+	    hl_release(heap, pointers[index]);
+	    pointers[index] = 0;
+	}
+    }
+
+    return SUCCESS;
 }
 
 /* Stress the heap library and see if you can break it!
  *
- * FUNCTIONS BEING TESTED:
- * INTEGRITY OR DATA CORRUPTION?
+ * FUNCTIONS BEING TESTED: alloc , release
+ * INTEGRITY OR DATA CORRUPTION? Data Integrity
  *
  * MANIFESTATION OF ERROR:
- *
+ * The library itself writes something into the block portion of the heap, which it should never do 
+ * unless that block has been freed by the user
  */
 int test19() {
 
-    return FAILURE;
+    int n_tries = 10000;
+    int block_size = 40;
+
+    char heap[HEAP_SIZE*3];
+    char *pointers[NPOINTERS];
+
+    //zero out the pointer array
+    memset(pointers, 0, NPOINTERS*sizeof(char *));
+
+    hl_init(heap, HEAP_SIZE*3);
+
+    char *array = (char *) hl_alloc(heap, HEAP_SIZE);
+    //set everything in array to A
+    for (int i=0; i<HEAP_SIZE; i++) {
+	array[i] = 'A';
+    }
+
+    //set pointers[0] to array
+    pointers[0] = array;
+
+    srandom(0);
+    for (int j=0; j<n_tries; j++) {
+	int index = random() % NPOINTERS;
+	if (index != 0) {
+	    if (pointers[index] == 0) {
+		pointers[index] = hl_alloc(heap, block_size);
+		//change the contents in pointers[index] to B
+		for (int t= 0; t<block_size && (pointers[index] != NULL); t++) {
+		    pointers[index][t] = 'B';
+		}
+	    } else {
+		hl_release(heap, pointers[index]);
+		pointers[index] = 0;
+	    }
+	}
+    }
+
+    bool testresult = SUCCESS;
+
+    //check if everything in pointers[0] is A
+    for (int k=0; k<HEAP_SIZE; k++) {
+	if(pointers[0][k] != 'A') {
+	    testresult = FAILURE;
+	}
+    }
+
+    return testresult;
 }
 
 
 /* Stress the heap library and see if you can break it!
  *
- * FUNCTIONS BEING TESTED:
- * INTEGRITY OR DATA CORRUPTION?
+ * FUNCTIONS BEING TESTED: alloc and resize
+ * INTEGRITY OR DATA CORRUPTION? Data Integrity
  *
  * MANIFESTATION OF ERROR:
- *
+ * The library itself writes something into the block portion of the heap, which it should never do
+ * unless that block has been freed by the user
  */
 int test20() {
 
-    return FAILURE;
+    int n_tries = 10000;
+    int block_size = 40;
+
+    char heap[HEAP_SIZE*3];
+    char *pointers[NPOINTERS];
+
+    //zero out the pointer array
+    memset(pointers, 0, NPOINTERS*sizeof(char *));
+    hl_init(heap, HEAP_SIZE*3);
+    char *array = (char *) hl_alloc(heap, HEAP_SIZE);
+
+    //set everything in array to A
+    for (int i =0; i<HEAP_SIZE; i++) {
+	array[i] = 'A';
+    }
+
+    //set pointers[0] to array
+    pointers[0] = array;
+
+    srandom(0);
+    for (int j=0; j<n_tries; j++) {
+	int index = random() % NPOINTERS;
+	if (index !=0) {
+	    if(pointers[index] == NULL) {
+		pointers[index] = hl_alloc(heap, block_size);
+		if (pointers[index] != NULL) {
+		    memset(pointers[index], 'B', block_size);
+		}
+	    } else {
+		int number = random() % (block_size*2);
+		pointers[index] = hl_resize(heap, pointers[index], number);
+		if (pointers[index] != NULL) {
+			memset(pointers[index], 'C', number);
+		}
+	    }
+	}
+    }
+
+    bool testresult = SUCCESS;
+
+    //check if everything in pointers[0] is A
+    for (int k = 0; k < HEAP_SIZE; k++) {
+	if(pointers[0][k] != 'A') {
+	    testresult = FAILURE;
+        }
+    }
+
+    return testresult;
 }
 
 
@@ -625,74 +726,27 @@ int test21() {
 
 /* Stress the heap library and see if you can break it!
  *
- * FUNCTIONS BEING TESTED: hl_alloc
- * INTEGRITY OR DATA CORRUPTION? Data corruption
+ * FUNCTIONS BEING TESTED:
+ * INTEGRITY OR DATA CORRUPTION?
  *
- * MANIFESTATION OF ERROR: we attempt to allocate more memory than
- * heap has space for. if the third alloc gets a non-null result,
- * the heap manager has allocated a block of memory that will
- * go beyond the end of the heap.
+ * MANIFESTATION OF ERROR:
  *
  */
 int test22() {
-    char heap[HEAP_SIZE];
-    hl_init(heap, HEAP_SIZE);
-	bool result = FAILURE;
-	
-	// we'll use a block size that we should be able to safely
-	// allocate 2 of, but not 3
-	int blocksize = HEAP_SIZE/2 - 128;
-	
-	// array of memory blocks we'll allocate and release
-	char *blocks[3];
-	
-	// first two should get valid pointers. third should not, because
-	// there is not enough room
-	blocks[0] = hl_alloc(heap, blocksize);
-	blocks[1] = hl_alloc(heap, blocksize);
-	blocks[2] = hl_alloc(heap, blocksize);
-	
-	DEBUG_PRINT_3("block0: %p, block1: %p, block2: %p\n", blocks[0], blocks[1], blocks[2]);
-	
-	if (blocks[2] != NULL) {
-		DEBUG_PRINT_2("End of Heap: %p, End of Block2: %p\n", &heap[0] + HEAP_SIZE, blocks[2] + blocksize);
-	}
-	
-	result = (blocks[2] == NULL) || (blocks[2] + blocksize <= &heap[0] + HEAP_SIZE);
-	
-    return result;
+
+    return FAILURE;
 }
 
 
 /* Stress the heap library and see if you can break it!
  *
- * FUNCTIONS BEING TESTED: hl_alloc, hl_release
- * INTEGRITY OR DATA CORRUPTION? Data corruption
+ * FUNCTIONS BEING TESTED:
+ * INTEGRITY OR DATA CORRUPTION?
  *
- * MANIFESTATION OF ERROR: 2 blocks are allocated. the first is released.
- * another bigger block is allocated. it should not overlap with the 2nd block allocated above.
+ * MANIFESTATION OF ERROR:
  *
  */
 int test23() {
-    char heap[HEAP_SIZE];
-    hl_init(heap, HEAP_SIZE);
-	
-	// array of memory blocks we'll allocate and release
-	char *blocks[3];
-	
-	// allocate some memory for 3 blocks
-	blocks[0] = hl_alloc(heap, 128);
-	blocks[1] = hl_alloc(heap, 128);
-	hl_release(heap, blocks[0]);
-	blocks[2] = hl_alloc(heap, 144);
-	
-	// we have a problem if hl_alloc put blocks[2] in the space
-	// it released for block[0], since there is not enough room
-	// there.
 
-	DEBUG_PRINT_3("block0: %p, block1: %p, block2: %p \n", blocks[0], blocks[1], blocks[2]);
-	
-	bool result = (blocks[2] > blocks[1]);
-
-    return result;
+     return FAILURE;
 }
